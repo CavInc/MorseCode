7b259488d059fc6037e85ac7c9abf5003a80ac4d
Alexander <cavinc20@gmail.com>
2020-04-03 16:08:39 +0700

    Работа над кодами

--- app/src/main/java/tk/cavinc/ui/MainActivity.java
+++ app/src/main/java/tk/cavinc/ui/MainActivity.java
@@ -219,6 +219,8 @@ public class MainActivity extends AppCompatActivity  {
     // https://overcoder.net/q/533848/%D0%BA%D0%B0%D0%BA-%D1%81%D0%B3%D0%B5%D0%BD%D0%B5%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D0%BE%D0%BF%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%83%D1%8E-%D1%87%D0%B0%D1%81%D1%82%D0%BE%D1%82%D1%83-%D0%B7%D0%B2%D1%83%D0%BA%D0%B0
     // https://habr.com/ru/post/126835/
 
+    // https://habr.com/ru/post/267605/
+
 
     private AudioTrack generateTone(double freqHz, int durationMs){
         int sampleRate = 48000;  // 44100 Hz
