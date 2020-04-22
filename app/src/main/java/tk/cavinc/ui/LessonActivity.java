74a044dac65562b8b5a182155533886eb6e765db
Alexander <cavinc20@gmail.com>
2020-04-22 16:59:02 +0700

    Работа над уроками

---
+++ app/src/main/java/tk/cavinc/ui/LessonActivity.java
@@ -0,0 +1,17 @@
+package tk.cavinc.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cav on 22.04.20.
+ */
+
+public class LessonActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
